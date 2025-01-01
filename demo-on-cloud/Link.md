a8fe4b7c05286667bc7bfd857ec863f2cd257d4a
nhatanh2709 <22520059@gm.uit.edu.vn>
2025-01-02 01:14:46 +0700

    update k8s mode release

--- demo-on-cloud/Link.md
+++
@@ -1,3 +0,0 @@
-# DEMO ON CLOUD
-
-[Google Drive File](https://drive.google.com/file/d/1p40rtNCu2HDGyF5mWNfF-Oo5gaZ09IG0/view?usp=sharing)
---
+++ k8s-seft-manager-aws-v2-istio/kubernetes-terraform/scripts/k8s/install-core-components.sh
@@ -0,0 +1,158 @@
+echo "#############################"
+echo "install-core-components.sh"
+echo "===== install aws-cli ====="
+sudo apt install unzip -y
+which unzip
+if [ $? -eq 0 ]; then
+    echo "Unzip installed successfully."
+else
+    echo "Unzip installation failed. Retrying..."
+    for i in {1..10}; do
+        sudo apt install unzip -y
+        which unzip
+        if [ $? -eq 0 ]; then
+            echo "Unzip installed successfully."
+            break
+        else
+            echo "Retrying in 5 seconds..."
+            sleep 5
+        fi
+    done
+fi
+
+
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -qq awscliv2.zip
+sudo ./aws/install
+
+#########################
+#ref: https://docs.docker.com/engine/install/ubuntu/
+echo "===== install containerd ====="
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release apt-transport-https
+sudo install -m 0755 -d /etc/apt/keyrings
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install containerd.io -y
+
+if [ $? -eq 0 ]; then
+    echo "containerd installed successfully."
+else
+    echo "containerd installation failed. Retrying..."
+    for i in {1..10}; do
+        sudo apt-get install containerd.io -y
+        if [ $? -eq 0 ]; then
+            echo "containerd installed successfully."
+            break
+        else
+            echo "Retrying in 5 seconds..."
+            sleep 5
+        fi
+    done
+fi
+
+#cd ~/
+#wget https://github.com/opencontainers/runc/releases/download/v1.1.2/runc.amd64
+#sudo install -m 755 runc.amd64 /usr/local/sbin/runc
+
+echo "===== install kubernetes components ====="
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+
+sudo kubeadm version
+if [ $? -eq 0 ]; then
+    echo "kubeadm installed successfully."
+else
+    echo "kubeadm installation failed. Retrying..."
+    for i in {1..10}; do
+        sudo apt-get install -y kubeadm
+        sudo kubeadm version
+        if [ $? -eq 0 ]; then
+            echo "kubeadm installed successfully."
+            break
+        else
+            echo "Retrying in 5 seconds..."
+            sleep 5
+        fi
+    done
+fi
+
+sudo kubelet --version
+if [ $? -eq 0 ]; then
+    echo "kubelet installed successfully."
+else
+    echo "kubelet installation failed. Retrying..."
+    for i in {1..10}; do
+        sudo apt-get install -y kubelet
+        sudo kubelet --version
+        if [ $? -eq 0 ]; then
+            echo "kubelet installed successfully."
+            break
+        else
+            echo "Retrying in 5 seconds..."
+            sleep 5
+        fi
+    done
+fi
+
+
+sudo apt-mark hold kubelet kubeadm kubectl
+
+echo "===== configure containerd ====="
+cat <<EOF | sudo tee /etc/crictl.yaml
+runtime-endpoint: unix:///run/containerd/containerd.sock
+image-endpoint: unix:///run/containerd/containerd.sock
+EOF
+
+cat <<EOF | sudo tee /etc/containerd/config.toml
+version = 2
+[plugins]
+  [plugins."io.containerd.grpc.v1.cri"]
+   [plugins."io.containerd.grpc.v1.cri".containerd]
+      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+          runtime_type = "io.containerd.runc.v2"
+          [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+            SystemdCgroup = true
+EOF
+
+#Forwarding IPv4 and letting iptables see bridged traffic
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
+sudo systemctl enable --now containerd
+sudo systemctl restart containerd
